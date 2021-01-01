1fbb0252e681821622b3014c0649b2b08f430acc
~HaimMi <rehaim1213@gmail.com>
2021-01-01 17:24:08 +0200

    files arrangement

--- utils/io.js
+++ utils/io.js
@@ -39,19 +39,3 @@ function printOutputLine(result) {
     var general = document.getElementById("general");
     general.appendChild(container);
 }
-
-document.addEventListener("DOMContentLoaded", function () {
-
-    printNewCommandLine();
-
-    // Execute a function when the user releases a key on the keyboard
-    document.addEventListener("keyup", function (event) {
-        // Number 13 is the "Enter" key on the keyboard
-        if (event.keyCode === 13) {
-            var getInput = document.getElementById(inputElementId - 1).value;
-            printOutputLine(getInput);
-            printNewCommandLine();
-        }
-    });
-
-});
